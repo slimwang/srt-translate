41f8eea76dfb846af47fc90a708d5fa7c87de5a7
slimwang <netbot@vip.qq.com>
2017-03-16 05:43:45 +0000

    refactored

--- translate.py
+++ translate.py
@@ -24,10 +24,6 @@ zh_file = open("zh-cn/" + "translated_" + file_name , "w")
 
 # translate
 sentence = []
-next_to_sentence = []
-en_position = 0
-zh_position = 0
-count = 0
 
 while True:
     line = en_file.readline()
@@ -36,13 +32,10 @@ while True:
     # write number and time axis
     if not check_contain_english(line):
         zh_file.write(line)
-        count += 0.5
 
     # add to sentence while not contain full stop
     elif not check_contain_full_stop(line):
         # save current file position
-        en_position = en_file.tell()
-        zh_position = zh_file.tell()
         sentence.append(line)
         
     # translate if sentence have a full stop
@@ -53,6 +46,8 @@ while True:
         return_str = en_blob.translate(to='zh')
         return_str = str(return_str)
         return_str = return_str.replace('，'," ").replace("。", "") # strip the ',' the API returned
+        
+        # write translation
         zh_file.write(return_str)
 
 
