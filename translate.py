a34cb5b2f6bd4cd8ca5316d22af967508f2ab8df
slimwang <netbot@vip.qq.com>
2017-03-16 05:23:52 +0000

    strip the punctuations

--- translate.py
+++ translate.py
@@ -1,10 +1,6 @@
+# -*- coding: UTF-8 -*-
 from textblob import TextBlob
 import re
-
-# en_blob = TextBlob('Now as you might have discovered,these four- or')
-# en_blob2 = TextBlob('five-letter symbols represent particular funds.')
-# print en_blob.translate(to='zh')
-# print en_blob2.translate(to='zh')
 
 def check_contain_english(check_str):
     for en in check_str:
@@ -17,6 +13,7 @@ def check_contain_full_stop(check_str):
         if s == ".":
             return True
     return False
+    
 
 # ====== extract English text and translate======
 file_name = "en-test.srt"
@@ -30,30 +27,36 @@ sentence = []
 next_to_sentence = []
 en_position = 0
 zh_position = 0
+count = 0
 
-for line in en_file.readlines():
+while True:
+    line = en_file.readline()
+    if not line:
+        break
     # write number and time axis
     if not check_contain_english(line):
         zh_file.write(line)
-        print line
-    
+        count += 0.5
+
     # add to sentence while not contain full stop
     elif not check_contain_full_stop(line):
         # save current file position
         en_position = en_file.tell()
         zh_position = zh_file.tell()
         sentence.append(line)
-        print "en_p:" + str(en_position)
-        print "zh_p:" + str(zh_position)
         
     # translate if sentence have a full stop
     else:
         sentence.append(line)
-        en_blob = TextBlob("".join(sentence).replace('\n','') )
+        en_blob = TextBlob("".join(sentence).replace('\n', " "))
         sentence = []
         return_str = en_blob.translate(to='zh')
-        zh_file.write(str(return_str))
-        
+        return_str = str(return_str)
+        return_str = return_str.replace('，'," ") # strip the ',' the API returned
+        zh_file.write(return_str)
+
+
+
 # close files
 en_file.close()
 zh_file.close()
