c6cb2674b2f11554cb1f3cd1fb259e46a1cdbc4f
slimwang <netbot@vip.qq.com>
2017-03-16 03:02:42 +0000

    whole sentence translation added

--- translate.py
+++ translate.py
@@ -11,18 +11,50 @@ def check_contain_english(check_str):
          if re.search('^[a-zA-Z]+$', en):
             return True
     return False
+    
+def check_contain_full_stop(check_str):
+    for s in check_str:
+        if s == ".":
+            return True
+    return False
 
-# extract English text
+# ====== extract English text and translate======
 file_name = "en-test.srt"
-with open("en-us/" + file_name,"r") as en_file:
-    with open("zh-cn/" + "output.srt", "w") as zh_file:
-        for line in en_file.readlines():
-            if not check_contain_english(line):
-                zh_file.write(line)
-                print line
-            else:
-                en_blob = TextBlob(line)
-                return_str = en_blob.translate(to='zh')
-                zh_file.write(str(return_str))
-                print str
+
+# open files
+en_file = open("en-us/" + file_name,"r")
+zh_file = open("zh-cn/" + "translated_" + file_name , "w")
+
+# translate
+sentence = []
+next_to_sentence = []
+en_position = 0
+zh_position = 0
+
+for line in en_file.readlines():
+    # write number and time axis
+    if not check_contain_english(line):
+        zh_file.write(line)
+        print line
+    
+    # add to sentence while not contain full stop
+    elif not check_contain_full_stop(line):
+        # save current file position
+        en_position = en_file.tell()
+        zh_position = zh_file.tell()
+        sentence.append(line)
+        print "en_p:" + str(en_position)
+        print "zh_p:" + str(zh_position)
+        
+    # translate if sentence have a full stop
+    else:
+        sentence.append(line)
+        en_blob = TextBlob("".join(sentence).replace('\n','') )
+        sentence = []
+        return_str = en_blob.translate(to='zh')
+        zh_file.write(str(return_str))
+        
+# close files
+en_file.close()
+zh_file.close()
         
