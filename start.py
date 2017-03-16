f986d7cbf2545a3263478c69e485de47c5ca9236
slimwang <netbot@vip.qq.com>
2017-03-16 11:57:07 +0000

    error handling added

--- start.py
+++ start.py
@@ -5,14 +5,25 @@ import translate as ts
 to_translate = sys.argv[1]
 translated = sys.argv[2]
 
+# get dir
 dir = str(to_translate)
 output_dir = str(translated)
 
+# read file list
 srt_files = []
+finished_files = []
+target_files = []
 for filename in os.listdir(dir):
     srt_files.append(filename)
+for filename in os.listdir(output_dir):
+    finished_files.append(filename)
 
-print srt_files
+# filter srt fils
+for s in srt_files:
+   if not s in finished_files:
+       target_files.append(s)
+            
 
-for file in srt_files:
+
+for file in target_files:
     ts.translate(file,dir,output_dir)
--- translate.py
+++ translate.py
@@ -18,95 +18,104 @@ def check_contain_full_stop(check_str):
     return False
     
 def translate(srt,dir,output_dir):
-    # ====== extract English text and translate======
-    file_name = srt
-    
-    # open files
-    en_file = open( dir + "/" + file_name,"r")
-    zh_file = open( output_dir + "/" + file_name , "w")
-    
-    # translate
-    sentence = []
-    dt = []
-    count = 0
-    
-    while True:
-        line = en_file.readline()
-        if not line:
-            break
+    try:
+        # ====== extract English text and translate======
+        file_name = srt
         
-        # write number and time axis
-        if not check_contain_english(line):
-            dt.append(line)
-    
-        # add to sentence while not contain full stop
-        elif not check_contain_full_stop(line):
-            sentence.append(line)
+        # open files
+        en_file = open( dir + "/" + file_name,"r")
+        zh_file = open( output_dir + "/" + file_name , "w")
+        
+        # translate
+        sentence = []
+        dt = []
+        count = 0
+        
+        while True:
+            line = en_file.readline()
+            if not line:
+                break
             
-        # translate if sentence have a full stop
-        else:
-            sentence.append(line)
-            en_blob = TextBlob("".join(sentence).replace('\n', " "))
-            sentence = []
-            return_str = en_blob.translate(to='zh')
-            return_str = str(return_str)
-            return_str = return_str.replace('，'," ").replace("。", "") # strip the ',' the API returned
-            
-            # split translation
-            dt = filter(lambda s: s != '\n', dt)
-            
-            short_lis = return_str.split()
-            sentence_count = len(dt) / 2
-            word_count = len(short_lis) / sentence_count
-            
-            # write to zh_file
-            if sentence_count == 1:
-                zh_file.write(dt[0])
-                zh_file.write(dt[1])
-                zh_file.write(return_str)
-                zh_file.write("\n")
-                zh_file.write("\n")
+            # write number and time axis
+            if not check_contain_english(line):
+                dt.append(line)
+        
+            # add to sentence while not contain full stop
+            elif not check_contain_full_stop(line):
+                sentence.append(line)
                 
-            if sentence_count == 2:
-                 for i in range(0,len(dt),2):
-                    zh_file.write(dt[i])
-                    zh_file.write(dt[i+1])
-                    if word_count == 1:
-                        if i == 0:
-                            zh_file.write(" ".join(short_lis[:1]))
-                        if i == 2:
-                            zh_file.write(" ".join(short_lis[1:]))
-                    if word_count == 2:
-                        if i == 0:
-                            zh_file.write(" ".join(short_lis[:2]))
-                        if i == 2:
-                            zh_file.write(" ".join(short_lis[2:]))
+            # translate if sentence have a full stop
+            else:
+                sentence.append(line)
+                en_blob = TextBlob("".join(sentence).replace('\n', " "))
+                sentence = []
+                return_str = en_blob.translate(to='zh')
+                return_str = str(return_str)
+                return_str = return_str.replace('，'," ").replace("。", "") # strip the ',' the API returned
+                
+                # split translation
+                dt = filter(lambda s: s != '\n', dt)
+                
+                short_lis = return_str.split()
+                sentence_count = len(dt) / 2
+                if sentence_count == 0:
+                    continue
+                word_count = len(short_lis) / sentence_count
+                
+                # write to zh_file
+                if sentence_count == 1:
+                    zh_file.write(dt[0])
+                    zh_file.write(dt[1])
+                    zh_file.write(return_str)
                     zh_file.write("\n")
                     zh_file.write("\n")
                     
-            if sentence_count == 3:
-                for i in range(0,len(dt),2):
-                    zh_file.write(dt[i])
-                    zh_file.write(dt[i+1])
-                    if word_count == 1:
-                        if i == 0:
-                            zh_file.write(" ".join(short_lis[:1]))
-                        if i == 2:
-                            zh_file.write(" ".join(short_lis[1:2]))
-                        if i == 4:
-                            zh_file.write(" ".join(short_lis[2:]))
-                    if word_count == 2:
-                        if i == 0:
-                            zh_file.write(" ".join(short_lis[:2]))
-                        if i == 2:
-                            zh_file.write(" ".join(short_lis[2:4]))
-                        if i == 4:
-                            zh_file.write(" ".join(short_lis[4:]))
-                    zh_file.write("\n")
-                    zh_file.write("\n")
-            dt = []
+                if sentence_count == 2:
+                     for i in range(0,len(dt),2):
+                        zh_file.write(dt[i])
+                        zh_file.write(dt[i+1])
+                        if word_count == 1:
+                            if i == 0:
+                                zh_file.write(" ".join(short_lis[:1]))
+                            if i == 2:
+                                zh_file.write(" ".join(short_lis[1:]))
+                        if word_count == 2:
+                            if i == 0:
+                                zh_file.write(" ".join(short_lis[:2]))
+                            if i == 2:
+                                zh_file.write(" ".join(short_lis[2:]))
+                        zh_file.write("\n")
+                        zh_file.write("\n")
+                        
+                if sentence_count == 3:
+                    for i in range(0,len(dt),2):
+                        zh_file.write(dt[i])
+                        zh_file.write(dt[i+1])
+                        if word_count == 1:
+                            if i == 0:
+                                zh_file.write(" ".join(short_lis[:1]))
+                            if i == 2:
+                                zh_file.write(" ".join(short_lis[1:2]))
+                            if i == 4:
+                                zh_file.write(" ".join(short_lis[2:]))
+                        if word_count == 2:
+                            if i == 0:
+                                zh_file.write(" ".join(short_lis[:2]))
+                            if i == 2:
+                                zh_file.write(" ".join(short_lis[2:4]))
+                            if i == 4:
+                                zh_file.write(" ".join(short_lis[4:]))
+                        zh_file.write("\n")
+                        zh_file.write("\n")
+                dt = []
+    except:
+        f = open("error.txt","w+")
+        f.write("script crashed with " + srt)
+    else:
+        # close files
+        en_file.close()
+        zh_file.close()
     
-    # close files
-    en_file.close()
-    zh_file.close()
+    # print
+    print str(srt) + " translated"
         
